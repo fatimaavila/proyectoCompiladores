e8271a5f2e77ce31c53dfc496c8e988e86539ab3
fatimaavila <fatimaavila@ufm.edu>
2022-09-06 07:23:57 -0600

    compiler class modif

--- compiler/Compiler.java
+++ compiler/Compiler.java
@@ -1,7 +1,7 @@
 class Compiler {
     public static void main(String[] args) {
       System.out.println("Compiler V 0.0001");
-      //System.out.println(args.length);
+      System.out.println(args.length);
       for (int i = 0; i < args.length; i++) {
           System.out.println("args["+i+"] "+args[i]);
       }  
