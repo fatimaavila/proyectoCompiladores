4bcdb3eb74dc086405839f11c90f4479fcf16190
Lorena Perez <lorenaperez@ufm.edu>
2022-10-04 18:05:58 -0600

    c

--- compiler/parser/sym.java
+++ compiler/parser/sym.java
@@ -9,44 +9,40 @@ package cupexample;
 public class sym {
   /* terminals */
   public static final int CLOSECURLYBRACES = 34;
-  public static final int DIVIDE = 5;
+  public static final int STRING_LITERAL = 25;
   public static final int READLINE = 36;
   public static final int FLOATINGPOINT = 23;
-  public static final int BOOLEANLITERAL = 24;
   public static final int UMINUS = 7;
+  public static final int MENORQ = 8;
+  public static final int DIFERENTE = 13;
   public static final int DOUBLE = 19;
-  public static final int NOTEQUAL = 13;
   public static final int CONTINUE = 59;
   public static final int PRIVATE = 50;
-  public static final int GREATER = 10;
   public static final int INTERFACE = 45;
   public static final int INT = 18;
   public static final int OPENCURLYBRACES = 33;
   public static final int FOR = 56;
-  public static final int LESS = 8;
-  public static final int MINUS = 3;
   public static final int SEMICOLON = 49;
   public static final int NOT = 16;
   public static final int AND = 14;
   public static final int READINTEGER = 35;
-  public static final int LESSEQUAL = 9;
+  public static final int IGUAL = 12;
   public static final int OR = 15;
   public static final int COMMA = 48;
-  public static final int BOOL = 20;
   public static final int CLASS = 44;
+  public static final int MAYORIGUALQ = 11;
   public static final int NEWARRAY = 38;
   public static final int OPENCLOSEBRACKET = 29;
-  public static final int PLUS = 2;
-  public static final int MULTIPLY = 4;
+  public static final int SUMA = 2;
   public static final int ASSIGN = 61;
   public static final int IF = 53;
   public static final int THIS = 62;
-  public static final int DOT = 17;
   public static final int CLOSEPARENTHESIS = 32;
   public static final int EOF = 0;
+  public static final int BOOLEAN = 20;
   public static final int DECIMAL = 22;
   public static final int RETURN = 57;
-  public static final int EQUAL = 12;
+  public static final int MENORIGUALQ = 9;
   public static final int NEW = 37;
   public static final int error = 1;
   public static final int CLOSEBRACKET = 28;
@@ -55,12 +51,14 @@ public class sym {
   public static final int BREAK = 58;
   public static final int VOID = 43;
   public static final int DTOI = 40;
-  public static final int GREATEREQUAL = 11;
   public static final int OPENPARENTHESIS = 31;
   public static final int ELSE = 54;
+  public static final int RESTAR = 3;
   public static final int PROTECTED = 51;
+  public static final int PUNTO = 17;
   public static final int WS = 30;
   public static final int OPENBRACKET = 27;
+  public static final int MAYORQ = 10;
   public static final int ITOD = 39;
   public static final int WHILE = 55;
   public static final int ITOB = 41;
@@ -68,37 +66,39 @@ public class sym {
   public static final int EXTENDS = 46;
   public static final int BTOI = 42;
   public static final int STRING = 21;
-  public static final int STRINGLITERAL = 25;
+  public static final int DIVISION = 5;
   public static final int IMPLEMENTS = 47;
   public static final int PRINT = 60;
+  public static final int BOOL_LITERAL = 24;
+  public static final int MULTIPLICACION = 4;
   public static final int IDENTIFIER = 63;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
-  "PLUS",
-  "MINUS",
-  "MULTIPLY",
-  "DIVIDE",
+  "SUMA",
+  "RESTAR",
+  "MULTIPLICACION",
+  "DIVISION",
   "MOD",
   "UMINUS",
-  "LESS",
-  "LESSEQUAL",
-  "GREATER",
-  "GREATEREQUAL",
-  "EQUAL",
-  "NOTEQUAL",
+  "MENORQ",
+  "MENORIGUALQ",
+  "MAYORQ",
+  "MAYORIGUALQ",
+  "IGUAL",
+  "DIFERENTE",
   "AND",
   "OR",
   "NOT",
-  "DOT",
+  "PUNTO",
   "INT",
   "DOUBLE",
-  "BOOL",
+  "BOOLEAN",
   "STRING",
   "DECIMAL",
   "FLOATINGPOINT",
-  "BOOLEANLITERAL",
-  "STRINGLITERAL",
+  "BOOL_LITERAL",
+  "STRING_LITERAL",
   "NULL",
   "OPENBRACKET",
   "CLOSEBRACKET",
